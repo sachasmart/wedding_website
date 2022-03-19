b2d9b3669ab18ffd1dd845d63d327e9f98849ee9
Sacha <sacha.smart@gmail.com>
2022-03-19 01:20:05 -0400

    Big update
    - refactored docker file, reduced docker's loading time, added nestjs user resource

--- api/bootstrap.sh
+++
@@ -1,6 +0,0 @@
-echo "Running Docker bootstrap script"
-
-cd /home/node/smart-api
-
-npm install
-npm run start:dev
--- api/src/modules/app/types/app.types.ts
+++
@@ -1,9 +0,0 @@
-/** API information */
-export interface IApiInfo {
-  /** API release date */
-  releaseDate?: string;
-  /** API Git release hash */
-  releaseHash?: string;
-  /** API version */
-  version: string;
-}
