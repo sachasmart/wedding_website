d147f12a295c7edb0bc9770f71b8c965000da330
Sacha <sacha.smart@gmail.com>
2022-03-22 22:44:14 -0400

    Update

--- src/user/models/user.entity.ts
+++ src/user/models/user.entity.ts
@@ -4,9 +4,18 @@ import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 export class UserEntity {
 
     @PrimaryGeneratedColumn()
-    id: number;
+    id: string;
 
     @Column()
     name: string;
+
+    // @Column({type: 'string', default: ""})
+    // phoneNumber: string;
+
+    // @Column({type: 'string', default: ""})
+    // email: string;
+
+    // @Column({ type: 'boolean', default: true })
+    // rsvp: boolean;
 
 }
